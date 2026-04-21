32335392d31786029ee96e6a1fb4893405ef33cf
pcnmargin <nguyenphan.ma@shondoshoes.com>
2026-04-21 17:05:13 +0700

    update

--- reports/report_top20.py
+++ reports/report_top20.py
@@ -8,7 +8,7 @@ from pathlib import Path
 import sys
 sys.path.insert(0, str(Path(__file__).parent.parent))
 from sqlalchemy import text
-from core.db import get_engine
+from core.db import get_engine, get_ecom_engine
 from core.sheets import get_client
 
 def main():
@@ -290,22 +290,7 @@ def main():
     combined_filter_sale = combined_filter
 
     # SALE ECOM 3 THÁNG GẦN NHẤT
-    # Lấy thông tin từ biến môi trường
-    host_ecom = os.getenv("DB_HOST_ECOM")
-    user_ecom = os.getenv("DB_USER_ECOM")
-    password_ecom = os.getenv("DB_PASSWORD_ECOM")
-    database_ecom = os.getenv("DB_NAME_ECOM")
-    port_ecom = os.getenv("DB_PORT_ECOM", 3306)
-
-    # Kết nối MySQL
-    connection_string_ecom = f"mysql+pymysql://{user_ecom}:{password_ecom}@{host_ecom}:{port_ecom}/{database_ecom}"
-
-    # Thêm pool_pre_ping=True và connect_args để tăng thời gian chờ
-    engine_ecom = create_engine(
-        connection_string_ecom,
-        pool_pre_ping=True,
-        connect_args={"connect_timeout": 30}  # tăng timeout từ mặc định (~10s) lên 20s
-    )
+    engine_ecom = get_ecom_engine()
 
     query_sales_90_days_ecom = """
         SELECT
